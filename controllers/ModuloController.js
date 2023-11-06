88f93b2a5d6daaa8c102b15632e9d5abed546428
Cristhian Bitencourt <cristhianthiagok@yahoo.com>
2023-11-06 11:14:13 -0300

    ordenação modulos

--- controllers/ModuloController.js
+++ controllers/ModuloController.js
@@ -61,11 +61,25 @@ const getAllModulos = async (req, res) => {
             })
             .exec();
 
+        // Ordenar os módulos
+        modulos.sort((a, b) => a.nome.localeCompare(b.nome));
+
+        // Ordenar os submódulos dentro de cada módul
+        modulos.forEach((modulo) => {
+            modulo.submodulos.sort((a, b) => a.nome.localeCompare(b.nome));
+
+            // Ordenar os processos de negócio dentro de cada submódulo
+            modulo.submodulos.forEach((submodulo) => {
+                submodulo.processosNegocio.sort((a, b) => a.nome.localeCompare(b.nome));
+            });
+        });
+
         res.status(200).json(modulos);
     } catch (error) {
         res.status(500).json({ error: "Não foi possível obter os módulos." });
     }
 };
+
 
 const deleteModulo = async (req, res) => {
     const moduloId = req.params.id;
