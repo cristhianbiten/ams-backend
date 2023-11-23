8bd8ceac5d386ac078610bf008c55da25c4e6f60
Cristhian Bitencourt <cristhianthiagok@yahoo.com>
2023-11-23 16:11:46 -0300

    validação de token

--- middlewares/authGuard.js
+++ middlewares/authGuard.js
@@ -1,26 +1,35 @@
 const User = require("../models/User");
-
 const jwt = require("jsonwebtoken");
-
 const jwtSecret = process.env.JWT_SECRET;
 
 const authGuard = async (req, res, next) => {
-    const authHeader = req.headers["authorization"];
-    const token = authHeader && authHeader.split(" ")[1];
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
 
-    // Check if header has a token
-    if (!token) return res.status(401).json({ errors: ["Acesso negado!"] });
+  // Check if header has a token
+  if (!token)
+    return res.status(401).json({ errors: ["Acesso negado!"] });
 
-    // Check if token is valid
-    try {
-        const verified = jwt.verify(token, jwtSecret);
+  // Check if token is valid
+  try {
+    const verified = jwt.verify(token, jwtSecret);
 
-        req.user = await User.findById(verified.id).select("-password");
+    // Set token expiration to 72 hours (3 days)
+    const expiresIn = 72 * 60 * 60; // 72 hours in seconds
 
-        next();
-    } catch (err) {
-        res.status(400).json({ errors: ["O Token é inválido!"] });
-    }
+    const user = await User.findById(verified.id).select("-password");
+
+    // Generate new token with updated expiration time
+    const newToken = jwt.sign({ id: user.id }, jwtSecret, { expiresIn });
+
+    // Send new token in response
+    res.setHeader("Authorization", `Bearer ${newToken}`);
+
+    req.user = user;
+    next();
+  } catch (err) {
+    res.status(400).json({ errors: ["O Token é inválido!"] });
+  }
 };
 
 module.exports = authGuard;
